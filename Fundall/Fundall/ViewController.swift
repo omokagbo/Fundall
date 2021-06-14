ad0e6a62dbc75779e2cb9422fc44b903b805e61f
Emmanuel Marabe Omokagbo <emmanuel.omokagbo@gmail.com>
2021-06-14 13:50:25 +0100

    create project workspace

---
+++ Fundall/Fundall/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fundall
+//
+//  Created by Decagon on 14/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
